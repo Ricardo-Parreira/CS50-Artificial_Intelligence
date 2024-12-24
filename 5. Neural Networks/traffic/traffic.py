13f2841cacb547c594a57a17365264aa14010901
Oferecido <ricardo.parreira5836@gmail.com>
2024-12-24 02:29:30 +0000

    parser done and submitted

--- 5. Neural Networks/traffic/traffic.py
+++ 5. Neural Networks/traffic/traffic.py
@@ -3,14 +3,18 @@ import numpy as np
 import os
 import sys
 import tensorflow as tf
+from tensorflow.keras import layers
 
 from sklearn.model_selection import train_test_split
 
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"  # Disable GPU
 EPOCHS = 10
 IMG_WIDTH = 30
 IMG_HEIGHT = 30
 NUM_CATEGORIES = 43
 TEST_SIZE = 0.4
+dataDir = "Home/Desktop/CS50_50/5.\ Neural\ Networks/traffic"
 
 
 def main():
@@ -85,8 +89,30 @@ def get_model():
     `input_shape` of the first layer is `(IMG_WIDTH, IMG_HEIGHT, 3)`.
     The output layer should have `NUM_CATEGORIES` units, one for each category.
     """
-    raise NotImplementedError
 
+    model = tf.keras.Sequential()
+
+    model.add(layers.InputLayer(input_shape = (IMG_WIDTH, IMG_HEIGHT, 3)))
+    model.add(layers.Conv2D(32, (3,3), activation='relu'))
+    model.add(layers.MaxPooling2D(pool_size=(2,2)))
+    model.add(layers.Conv2D(32, (3,3), activation='relu'))
+    model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+    model.add(layers.Conv2D(32, (3, 3), activation='sigmoid'))
+    model.add(layers.Flatten())
+
+
+    model.add(layers.Dense(NUM_CATEGORIES, activation='softmax'))
+
+
+    model.compile(
+        optimizer='adam',
+        # Loss function to minimize
+        loss='categorical_crossentropy',
+        # List of metrics to monitor
+        metrics=['accuracy'],
+    )
+
+    return model
 
 if __name__ == "__main__":
     main()
